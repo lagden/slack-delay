4c5c8c241ac3403fc53bef9091320389d95d202b
Thiago Lagden <lagden@gmail.com>
2017-05-30 22:25:21 -0300

    added mkdir

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const {join} = require('path')
 const {RtmClient, WebClient, MemoryDataStore, RTM_EVENTS, CLIENT_EVENTS} = require('@slack/client')
 const promisify = require('lagden-promisify')
 const got = require('got')
+const mkdirp = require('mkdirp')
 const debug = require('./lib/debug')
 const {read} = require('./lib/helpers')
 const server = require('./lib/server')
@@ -20,6 +21,9 @@ const WEB_TOKEN = process.env.SLACK_API_TOKEN || config.SLACK_API_TOKEN
 const BOT_TOKEN = process.env.SLACK_BOT_TOKEN || config.SLACK_BOT_TOKEN
 const DELAY = process.env.DELAY || config.DELAY || 10000        // in milliseconds
 const INTERVAL = process.env.INTERVAL || config.INTERVAL || 10  // in seconds
+const dataDir = join(__dirname, 'data')
+
+mkdirp.sync(dataDir)
 
 debug.log(DELAY)
 debug.log(INTERVAL)
@@ -40,7 +44,7 @@ function clear() {
 }
 
 async function triggerMsg(file) {
-	const filepath = join(__dirname, 'data', `${file}`)
+	const filepath = join(dataDir, `${file}`)
 	const data = await readFile(filepath)
 	const msg = JSON.parse(data)
 	if (msg && msg.text) {
@@ -62,7 +66,7 @@ function loop() {
 	intervalID = setInterval(() => {
 		read(DELAY)
 			.then(files => {
-				console.log(files)
+				debug.log('files to trigger --->', files)
 				return Promise.all(files.map(triggerMsg))
 			})
 			.then(r => {
@@ -79,7 +83,7 @@ function loop() {
 rtm.on(RTM_EVENTS.MESSAGE, message => {
 	debug.log(message.channel, origem, message.type)
 	if (message.channel === origem && message.type === 'message') {
-		const file = join(__dirname, 'data', `${Date.now()}.json`)
+		const file = join(dataDir, `${Date.now()}.json`)
 		const ws = fs.createWriteStream(file)
 		ws.on('error', err => {
 			debug.error(err.message)
@@ -93,7 +97,7 @@ rtm.on(RTM_EVENTS.MESSAGE, message => {
 })
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, loop)
-// rtm.on(CLIENT_EVENTS.RTM.ATTEMPTING_RECONNECT, clear)
+rtm.on(CLIENT_EVENTS.RTM.ATTEMPTING_RECONNECT, clear)
 rtm.on(CLIENT_EVENTS.RTM.DISCONNECT, clear)
 
 rtm.start()
